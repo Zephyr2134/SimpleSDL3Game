d0d4bc786f7648dc2d2368780b3f8023ecec366f
Zevantra <nikolaistanes@gmail.com>
2026-02-18 14:28:12 +0100

    I think it works

--- ECS/MicrophoneComponent.hpp
+++ ECS/MicrophoneComponent.hpp
@@ -2,6 +2,7 @@
 #include "Components.hpp"
 #include <SDL3/SDL.h>
 #include <iostream>
+#include <cmath>
 
 class MicrophoneComponent : public Component
 {
@@ -17,41 +18,53 @@ public:
 
     void init() override
     {
-        spec.channels = 1;
-        spec.format = SDL_AUDIO_F32;
-        spec.freq = 8000;
-        stream = SDL_OpenAudioDeviceStream(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, &spec, NULL, NULL);
-        if (!stream) {
-            SDL_Log("Couldn't create audio stream: %s", SDL_GetError());
-            return;
-        }
-        SDL_ResumeAudioStreamDevice(stream);
+        mic =
+        SDL_OpenAudioDevice(SDL_AUDIO_DEVICE_DEFAULT_RECORDING, NULL);
+
+        SDL_GetAudioDeviceFormat(mic, &src, NULL);
+
+        dst.freq = 48000;
+        dst.channels = 1;
+        dst.format = SDL_AUDIO_F32;
+
+    stream = SDL_CreateAudioStream(&src, &dst);
+    SDL_BindAudioStream(mic, stream);
+    SDL_ResumeAudioDevice(mic);
     }
 
     void update() override
     {
-        minimum_audio = (8000 * sizeof (float)) / 2;
-         if (SDL_GetAudioStreamQueued(stream) < minimum_audio) {
-            float samples[512];
+        float samples[1024];
 
-            for (int i = 0; i < SDL_arraysize(samples); i++) {
-            const int freq = 440;
-            const float phase = currentSineSample * freq / 8000.0f;
-            samples[i] = SDL_sinf(phase * 2 * SDL_PI_F);
-            currentSineSample++;
+        int bytes = SDL_GetAudioStreamData(stream, samples, sizeof(samples));
+        if (bytes <= 0)
+            return;
+
+        int frames = bytes / sizeof(float);
+        if (frames == 0)
+            return;
+
+        /*float rms = 0.0f;
+        for (int i = 0; i < frames; i++)
+            rms += samples[i] * samples[i];
+
+        rms = sqrt(rms / frames);
+        std::cout << rms << std::endl;*/
+        int crossings = 0;
+        for (int i = 1; i < frames; i++) {
+            if (samples[i-1] < 0 && samples[i] >= 0)
+                crossings++;
         }
+        float frequency = (crossings * 48000) / (2.0f * frames);
+        std::cout << frequency << std::endl;
 
-        currentSineSample %= 8000;
-
-        SDL_PutAudioStreamData(stream, samples, sizeof (samples));
-    }
     }
 
     void draw() override {}
 
 private:
+    SDL_AudioDeviceID mic;
     SDL_AudioStream* stream;
-    SDL_AudioSpec spec;
-    int currentSineSample;
-    int minimum_audio;
+    SDL_AudioSpec src;
+    SDL_AudioSpec dst;
 };
